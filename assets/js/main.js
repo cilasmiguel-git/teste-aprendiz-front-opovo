b05abd0aa098a0873194e0c3332e5842db73ff5e
cilasmiguel-git <miguelcilascolacobezerramiguel@gmail.com>
2026-04-02 15:12:10 -0300

    feat: setup global CSS tokens, base styles and import assets

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo principal de scripts da Landing Page
+ * Desenvolvido como parte do teste para o Grupo O POVO
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Nota: Inicializações do jQuery e OwlCarousel foram enviadas dinamicamente
+    // direto para dentro do lifecycle (connectedCallback) dos Web Components 
+    // específicos que usam o Carrossel (Elenco.js e Resenhas.js).
+
+    console.log("O POVO: Interface carregada via Vanilla Web Components e CSS Modular com Sucesso!");
+});
